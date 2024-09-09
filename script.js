0f24c064495f0c69f9031348db227564c3624447
Srilashman <srilashman.sg@gmail.com>
2024-09-09 08:31:07 +0800

    Add error message for empty name in index.html

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     enter_button.addEventListener("click", function() {
         if (input.value.length === 0) {
+            error.textContent = "NAME REQUIRED";
             error.style.display = "block"; // Show error message if input is empty
-        } else {
+        } else if(!name_check(input.value)) {
+            error.textContent = "NAME CANNOT BE EMPTY";
+            error.style.display = "block";
+        }
+        else {
             error.style.display = "none";  // Hide error message if input is not empty
             window.location.href = "placings.html?name=" + input.value;
         }
@@ -21,5 +26,10 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
     });
-
+    function name_check(name){
+        for(let i = 0; i < name.length; i++){
+            if(name[i] !== ' ') return true; //name does not have spaces
+        }
+        return false; //name is just spaces
+    }
 });
